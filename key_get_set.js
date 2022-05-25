ddc050d2dcc5ec28bace8b4d478e1fbe809e2921
Jessica Classon <jessicaclasson4@gmail.com>
2022-05-25 14:34:34 -0400

    redis-version
    redis version changed: npm i redis@3.0.0

--- key_get_set.js
+++ key_get_set.js
@@ -8,8 +8,9 @@ client.get("my_key", function(err, reply) {
 });
 
 // multiple value write & read
-client.mSet('header', 0, 'left',0, 'article',0, 'right',0, 'footer',0);
-client.mGet(['header', 'left', 'article', 'right', 'footer'],
+client.mset('header', 0, 'left',0, 'article',0, 'right',0, 'footer',0);
+client.mget(['header', 'left', 'article', 'right', 'footer'],
      function( err, value) {
         console.log(value);
  });
+client.quit();
