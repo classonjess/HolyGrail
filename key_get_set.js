a84561de71ba9ae69a8a377d3b3366aa47702c08
Jessica Classon <jessicaclasson4@gmail.com>
2022-05-25 21:00:42 -0400

    back end and front end connection

--- key_get_set.js
+++ key_get_set.js
@@ -1,6 +1,7 @@
 var redis = require("redis");
 var client = redis.createClient();
 
+
 // single value write & read
 client.set("my_key", "Hello Wold!");
 client.get("my_key", function(err, reply) {
--- public/index.js
+++ public/index.js
@@ -47,12 +47,20 @@ function read() {
 function App(){
     const [data, setData] = React.useState({header:0,left:0,article:0,right:0,footer:0});
 
+    React.useEffect(() => {
+        //read db data &update UI
+        const response = read()
+              .then(res => {
+                  setData(res)
+              });
+    })
+
     function handle(section){
-        console.log("Pong", section);
-        const value = data[section.name] + section.value;
-        const object = {[section.name]:value};
-        setData({...data, ...object});
-    }
+       //update db & local state
+       const response = update(section.name, section.value)
+            .then(res => {
+                setData(res)
+            });
 
     return (<>
        <div className="grid">
@@ -68,4 +76,4 @@ function App(){
 ReactDOM.render(
     <App/>,
     document.getElementById('root')
-);
+)}
